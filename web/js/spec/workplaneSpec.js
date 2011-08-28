7f2ec53af225e604981c0e060133d07ef3076d09
Benjamin Nortier <bjnortier@gmail.com>
2011-08-28 21:06:25 +0100

    Fixed Jasmine test

--- web/js/spec/workplaneSpec.js
+++ web/js/spec/workplaneSpec.js
@@ -2,7 +2,7 @@ describe("gridextents", function() {
 
     it("should compute a position inside the X and Y extents", function() {
 	
-	var gridExtents = SS.gridExtents({minX: -10, minY: -5, maxX: 7, maxY: 3});
+	var gridExtents = SS.workplane.gridExtents({minX: -10, minY: -5, maxX: 7, maxY: 3});
 
 	expect(gridExtents.isInsideX(-10.1)).toEqual(false);
 	expect(gridExtents.isInsideX(-10)).toEqual(true);
