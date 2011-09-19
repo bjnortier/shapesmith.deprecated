afe498fc7914beaceff42ff1bff035cd9efc396d
Benjamin Nortier <bjnortier@gmail.com>
2011-09-19 21:24:02 +0100

    Refactored common preview constructors

--- web/js/src/constructors.js
+++ web/js/src/constructors.js
@@ -2,71 +2,76 @@ var SS = SS || {};
 
 SS.constructors = {};
 
-SS.constructors.sphere = function(spec) {
+SS.constructors.originMixin = function(shared) {
     var that = {};
 
-    var focussed, previewGeometry;
-
-    var onWorkplaneCursor = function(event) {
-	
-	if (focussed === 'origin') {
-	    
+    that.onWorkplaneCursorUpdated = function (event) {
+	if (shared.focussed === 'origin') {
 	    $('#x').val(event.x);
 	    $('#y').val(event.y);
-	    updatePreview();
-
-	} else if (focussed === 'r') {
-	    var originX = $('#x').val(), originY = $('#y').val();
-	    var x = event.x, y = event.y, r;
-	    
-	    var dx = Math.abs(x - originX), dy = Math.abs(y - originY);
-	    var r  = Math.sqrt(dx*dx + dy*dy);
-	    
-	    $('#r').val(r.toFixed(2));
-	    updatePreview();
+	    return true;
 	}
+	return false;
     }
 
-    var onWorkplaneClicked = function(event) {
-	if (focussed === 'origin') {
-	    $('#modal-ok').focus();
-	} else if (focussed === 'r') {
-	    $('#modal-ok').focus();
-	}
+    that.setupValueHandlers = function() {
+	$('#x').change(shared.updatePreview);
+	$('#y').change(shared.updatePreview);
+	$('#z').change(shared.updatePreview);
+    }
+
+    that.setupFocusHandlers = function() {
+	$('#x').focus(function() { shared.focussed = 'origin'; });
+	$('#y').focus(function() { shared.focussed = 'origin'; });
+	$('#z').focus(function() { shared.focussed = 'origin'; });
+
+	$('#x').blur(function() { shared.focussed = undefined; });
+	$('#y').blur(function() { shared.focussed = undefined; });
+	$('#z').blur(function() { shared.focussed = undefined; });
+
+	$('#modal-ok').focus(function() { shared.focussed = 'ok'; });
+	$('#model-ok').blur(function()  { shared.focussed = undefined; });
+    }
+
+    that.create = function() {
+	var lastMousePosition = sceneView.workplane.getLastMousePosition();
+
+	shared.setupValueHandlers();
+	shared.setupFocusHandlers();
+
+	var originX = lastMousePosition.x;
+	var originY = lastMousePosition.y;
+	$('#x').val(originX);
+	$('#y').val(originY);
+
+	sceneView.workplane.on('workplaneCursorUpdated', shared.onWorkplaneCursorUpdated);
+	sceneView.workplane.on('workplaneClicked', shared.onWorkplaneClicked);
+
+    }
+    
+    that.dispose = function() {
+	sceneView.workplane.off('workplaneCursorUpdated', shared.onWorkplaneCursorUpdated);
+	sceneView.workplane.off('workplaneClicked', shared.onWorkplaneClicked);
 	
+	if (shared.previewGeometry) {
+	    sceneView.scene.removeObject(shared.previewGeometry);
+	}
+	SS.constructors.active = null;
     }
 
-    var setupValueHandlers = function() {
-	$('#x').change(updatePreview);
-	$('#y').change(updatePreview);
-	$('#z').change(updatePreview);
-	$('#r').change(updatePreview);
-    }
+    return that;
+}
 
+SS.constructors.sphere = function() {
+    var that = {};
+    var shared = {}, originMixin; 
 
-    var setupFocusHandlers = function() {
-	$('#r').focus(function() { focussed = 'r'; });
-	$('#r').blur(function()  { focussed = undefined; });
-
-	$('#x').focus(function() { focussed = 'origin'; });
-	$('#y').focus(function() { focussed = 'origin'; });
-	$('#z').focus(function() { focussed = 'origin'; });
-
-	$('#x').blur(function() { focussed = undefined; });
-	$('#y').blur(function() { focussed = undefined; });
-	$('#z').blur(function() { focussed = undefined; });
-
-	$('#modal-ok').focus(function() { focussed = 'ok'; });
-	$('#model-ok').blur(function()  { focussed = undefined; });
-	
-    }
-
-    var updatePreview = function() {
-	if (previewGeometry) {
-	    sceneView.scene.removeObject(previewGeometry);
+    shared.updatePreview = function() {
+	if (shared.previewGeometry) {
+	    sceneView.scene.removeObject(shared.previewGeometry);
 	}
 
-	previewGeometry = new THREE.Object3D();
+	shared.previewGeometry = new THREE.Object3D();
 
 	var x = parseFloat($('#x').val());
 	var y = parseFloat($('#y').val());
@@ -75,7 +80,7 @@ SS.constructors.sphere = function(spec) {
 	var geometry = new THREE.SphereGeometry(r, 50, 50);
 	var material = new THREE.MeshBasicMaterial({color: 0x3F8FD2, opacity: 0.5});
 	var sphere = new THREE.Mesh(geometry, material);
-	previewGeometry.addChild(sphere);
+	shared.previewGeometry.addChild(sphere);
 
 	var circleGeom = new THREE.Geometry();
 	for(var i = 0; i <= 50; ++i) {
@@ -85,41 +90,58 @@ SS.constructors.sphere = function(spec) {
 	    circleGeom.vertices.push(new THREE.Vertex(new THREE.Vector3(dx, dy, 0)));
 	}
 	var circle = new THREE.Line(circleGeom, new THREE.LineBasicMaterial({ color: 0xffffff, opacity: 0.5 }));
-	previewGeometry.addChild(circle);
+	shared.previewGeometry.addChild(circle);
     
-	previewGeometry.position.x = x;
-	previewGeometry.position.y = y;
-	sceneView.scene.addObject(previewGeometry);
+	shared.previewGeometry.position.x = x;
+	shared.previewGeometry.position.y = y;
+	sceneView.scene.addObject(shared.previewGeometry);
     }
-    
+
+    shared.onWorkplaneCursorUpdated = function(event) {
+
+	if (originMixin.onWorkplaneCursorUpdated(event)) {
+	    shared.updatePreview();
+	} else if (shared.focussed === 'r') {
+	    var originX = $('#x').val(), originY = $('#y').val();
+	    var x = event.x, y = event.y;
+	    
+	    var dx = Math.abs(x - originX), dy = Math.abs(y - originY);
+	    var r  = Math.sqrt(dx*dx + dy*dy);
+	    
+	    $('#r').val(r.toFixed(2));
+	    shared.updatePreview();
+	}
+    }
+
+    shared.onWorkplaneClicked = function(event) {
+	if (shared.focussed === 'origin') {
+	    $('#modal-ok').focus();
+	} else if (shared.focussed === 'r') {
+	    $('#modal-ok').focus();
+	}
+    }
+
+    shared.setupValueHandlers = function() {
+	originMixin.setupValueHandlers();
+	$('#r').change(shared.updatePreview);
+    }
+
+    shared.setupFocusHandlers = function() {
+	originMixin.setupFocusHandlers();
+	$('#r').focus(function() { shared.focussed = 'r'; });
+	$('#r').blur(function()  { shared.focussed = undefined; });
+    }
+
+    var originMixin = this.originMixin(shared);
+
     that.create = function() {
-	var lastMousePosition = sceneView.workplane.getLastMousePosition();
-
-	setupValueHandlers();
-	setupFocusHandlers();
-
-	var originX = lastMousePosition.x;
-	var originY = lastMousePosition.y;
-	$('#x').val(originX);
-	$('#y').val(originY);
-
+	originMixin.create();
 	$('#r').focus();
-	
-	sceneView.workplane.on('workplaneCursorUpdated', onWorkplaneCursor);
-	sceneView.workplane.on('workplaneClicked', onWorkplaneClicked);
-
 	SS.constructors.active = this;
     }
 
     that.dispose = function() {
-	sceneView.workplane.off('workplaneCursorUpdated', onWorkplaneCursor);
-	sceneView.workplane.off('workplaneClicked', onWorkplaneClicked);
-	
-	if (previewGeometry) {
-	    sceneView.scene.removeObject(previewGeometry);
-	}
-	SS.constructors.active = null;
-
+	originMixin.dispose();
     }
     
     return that;
@@ -127,80 +149,14 @@ SS.constructors.sphere = function(spec) {
 
 SS.constructors.cylinder = function(spec) {
     var that = {};
+    var shared = {}, originMixin;
 
-    var focussed, previewGeometry;
+    shared.updatePreview = function() {
 
-    var onWorkplaneCursor = function(event) {
-	
-	if (focussed === 'origin') {
-	    
-	    $('#x').val(event.x);
-	    $('#y').val(event.y);
-	    updatePreview();
-
-	} else if (focussed === 'r') {
-	    var originX = $('#x').val(), originY = $('#y').val();
-	    var x = event.x, y = event.y, r;
-	    
-	    var dx = Math.abs(x - originX), dy = Math.abs(y - originY);
-	    var r  = Math.sqrt(dx*dx + dy*dy);
-	    
-	    $('#r').val(r.toFixed(2));
-	    updatePreview();
-	} else if (focussed === 'h') {
-	    updatePreview();
+	if (shared.previewGeometry) {
+	    sceneView.scene.removeObject(shared.previewGeometry);
 	}
-
-    }
-
-    var onWorkplaneClicked = function(event) {
-	if (focussed === 'origin') {
-	    $('#modal-ok').focus();
-	} else if (focussed === 'r') {
-	    $('#h').focus();
-	} else if (focussed === 'h') {
-	    $('#modal-ok').focus();
-	}
-
-	
-    }
-
-    var setupValueHandlers = function() {
-	$('#x').change(updatePreview);
-	$('#y').change(updatePreview);
-	$('#z').change(updatePreview);
-	$('#r').change(updatePreview);
-	$('#h').change(updatePreview);
-
-    }
-
-
-    var setupFocusHandlers = function() {
-	$('#r').focus(function() { focussed = 'r'; });
-	$('#r').blur(function()  { focussed = undefined; });
-
-	$('#h').focus(function() { focussed = 'h'; });
-	$('#h').blur(function()  { focussed = undefined; });
-
-	$('#x').focus(function() { focussed = 'origin'; });
-	$('#y').focus(function() { focussed = 'origin'; });
-	$('#z').focus(function() { focussed = 'origin'; });
-
-	$('#x').blur(function() { focussed = undefined; });
-	$('#y').blur(function() { focussed = undefined; });
-	$('#z').blur(function() { focussed = undefined; });
-
-	$('#modal-ok').focus(function() { focussed = 'ok'; });
-	$('#model-ok').blur(function()  { focussed = undefined; });
-	
-    }
-
-    var updatePreview = function() {
-	if (previewGeometry) {
-	    sceneView.scene.removeObject(previewGeometry);
-	}
-
-	previewGeometry = new THREE.Object3D();
+	shared.previewGeometry = new THREE.Object3D();
 
 	var x = parseFloat($('#x').val());
 	var y = parseFloat($('#y').val());
@@ -212,7 +168,7 @@ SS.constructors.cylinder = function(spec) {
 	    var material = new THREE.MeshBasicMaterial({color: 0x3F8FD2, opacity: 0.5});
 	    var cylinder = new THREE.Mesh(geometry, material);
 	    cylinder.position.z = h/2;
-	    previewGeometry.addChild(cylinder);
+	    shared.previewGeometry.addChild(cylinder);
 	}
 
 	var circleGeom1 = new THREE.Geometry(), circleGeom2 = new THREE.Geometry();
@@ -224,49 +180,86 @@ SS.constructors.cylinder = function(spec) {
 	    circleGeom2.vertices.push(new THREE.Vertex(new THREE.Vector3(dx, dy, 0)));
 	}
 	var circle1 = new THREE.Line(circleGeom1, new THREE.LineBasicMaterial({ color: 0xffffff, opacity: 0.5 }));
-	previewGeometry.addChild(circle1);
+	shared.previewGeometry.addChild(circle1);
 
 	if (h) {
 	    var circle2 = new THREE.Line(circleGeom2, new THREE.LineBasicMaterial({ color: 0xffffff, opacity: 0.5 }));
 	    circle2.position.z = h;
-	    previewGeometry.addChild(circle2);
+	    shared.previewGeometry.addChild(circle2);
 	}
 
-	previewGeometry.position.x = x;
-	previewGeometry.position.y = y;
+	shared.previewGeometry.position.x = x;
+	shared.previewGeometry.position.y = y;
 
-	sceneView.scene.addObject(previewGeometry);
+	sceneView.scene.addObject(shared.previewGeometry);
+	
     }
+
+    shared.onWorkplaneCursorUpdated = function(event) {
+	
+	if (originMixin.onWorkplaneCursorUpdated(event)) {
+	    shared.updatePreview();
+	} else if (shared.focussed === 'r') {
+	    var originX = $('#x').val(), originY = $('#y').val();
+	    var x = event.x, y = event.y, r;
+	    
+	    var dx = Math.abs(x - originX), dy = Math.abs(y - originY);
+	    var r  = Math.sqrt(dx*dx + dy*dy);
+	    
+	    $('#r').val(r.toFixed(2));
+	    shared.updatePreview();
+	} else if (shared.focussed === 'h') {
+	    shared.updatePreview();
+	}
+
+    }
+
+    shared.onWorkplaneClicked = function(event) {
+	if (shared.focussed === 'origin') {
+	    if ($('#h').val() === '') {
+		$('#h').focus();
+	    } else {
+		$('#modal-ok').focus();
+	    }
+	} else if (shared.focussed === 'r') {
+	    if ($('#h').val() === '') {
+		$('#h').focus();		
+	    } else {
+		$('#modal-ok').focus();
+	    }
+	} else if (shared.focussed === 'h') {
+	    $('#modal-ok').focus();
+	}
+    }
+
+    shared.setupValueHandlers = function() {
+	originMixin.setupValueHandlers(shared.updatePreview);
+	$('#r').change(shared.updatePreview);
+	$('#h').change(shared.updatePreview);
+    }
+
+    shared.setupFocusHandlers = function() {
+	originMixin.setupFocusHandlers();
+
+	$('#r').focus(function() { shared.focussed = 'r'; });
+	$('#r').blur(function()  { shared.focussed = undefined; });
+
+	$('#h').focus(function() { shared.focussed = 'h'; });
+	$('#h').blur(function()  { shared.focussed = undefined; });
+    }
+
+    var originMixin = this.originMixin(shared);
     
     that.create = function() {
-	var lastMousePosition = sceneView.workplane.getLastMousePosition();
-
-	setupValueHandlers();
-	setupFocusHandlers();
-
-	var originX = lastMousePosition.x;
-	var originY = lastMousePosition.y;
-	$('#x').val(originX);
-	$('#y').val(originY);
-
+	originMixin.create();
 	$('#r').focus();
-	
-	sceneView.workplane.on('workplaneCursorUpdated', onWorkplaneCursor);
-	sceneView.workplane.on('workplaneClicked', onWorkplaneClicked);
-
 	SS.constructors.active = this;
     }
 
     that.dispose = function() {
-	sceneView.workplane.off('workplaneCursorUpdated', onWorkplaneCursor);
-	sceneView.workplane.off('workplaneClicked', onWorkplaneClicked);
-	
-	if (previewGeometry) {
-	    sceneView.scene.removeObject(previewGeometry);
-	}
-	SS.constructors.active = null;
-
+	originMixin.dispose();
     }
     
     return that;
 }
+
