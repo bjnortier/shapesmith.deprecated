386f88ca6d7809863978fc3b55b8e3857a6b31d5
Benjamin Nortier <bjnortier@gmail.com>
2011-12-09 18:42:49 +0200

    Design name validation

--- static/js/src/designs.js
+++ static/js/src/designs.js
@@ -1,23 +1,4 @@
 
-$('#create-design-button').click(function() {
-    var newDesignName = $('#new-name').val();
-    $.ajax({
-        type: 'POST',
-	url: '/' + SS.session.username + '/' + newDesignName + '/',
-	data: '{}',
-	dataType: 'json',
-	contentType: 'application/json',
-	success: function(response) {
-	    console.log(response);
-	    window.location.href = "/{{username}}/" + newDesignName + "/modeller.html?ref=heads.master";
-	},
-	error: function(response) {
-	    $('#new-name').css('border', 'solid thin red');
-	    console.log(response);
-        }
-    });
-});
-
 SS.delete_design = function(name) {
     var answer = confirm ("This will permanently delete the design. Are you sure?")
     if (answer) {
@@ -41,10 +22,40 @@ SS.update_empty = function() {
     }    
 }
 
+SS.new_name_error = function(error) {
+    $('#new-name').css('border', 'solid thin red');
+    $('#design-name-errors').text(error)
+    $('#design-name-errors').show();
+}
+
+$('#create-design-button').click(function() {
+    var newDesignName = $('#new-name').val().trim();
+    if (newDesignName === "") {
+	SS.new_name_error('Required');
+	return;
+    }
+    $.ajax({
+        type: 'POST',
+	url: '/' + SS.session.username + '/' + newDesignName + '/',
+	data: '{}',
+	dataType: 'json',
+	contentType: 'application/json',
+	success: function(response) {
+	    console.log(response);
+	    window.location.href = "/{{username}}/" + newDesignName + "/modeller.html?ref=heads.master";
+	},
+	error: function(response) {
+	    SS.new_name_error(JSON.parse(response.responseText));
+        }
+    });
+});
+
+
 $(document).ready(function() {
     var sameSizeFn = function() {
         $('#new-name').width($('#design-name').width() - 20);
         $('#new-name').height($('#create-design-button').height());
+        $('#design-name-errors').width($('#design-name').width() - 20);
     };
     $(window).resize(sameSizeFn);
     sameSizeFn();
