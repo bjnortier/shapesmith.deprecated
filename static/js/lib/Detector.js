f076c716482f4c1b930d259e2509c8126d41708a
Benjamin Nortier <bjnortier@gmail.com>
2012-01-11 10:43:09 +0000

    WebGL detector message

--- static/js/lib/Detector.js
+++ static/js/lib/Detector.js
@@ -5,54 +5,47 @@
 
 Detector = {
 
-	canvas : !! window.CanvasRenderingContext2D,
-	webgl : ( function () { try { return !! window.WebGLRenderingContext && !! document.createElement( 'canvas' ).getContext( 'experimental-webgl' ); } catch( e ) { return false; } } )(),
-	workers : !! window.Worker,
-	fileapi : window.File && window.FileReader && window.FileList && window.Blob,
+    canvas : !! window.CanvasRenderingContext2D,
+    webgl : ( function () { try { return !! window.WebGLRenderingContext && !! document.createElement( 'canvas' ).getContext( 'experimental-webgl' ); } catch( e ) { return false; } } )(),
+    workers : !! window.Worker,
+    fileapi : window.File && window.FileReader && window.FileList && window.Blob,
 
-	getWebGLErrorMessage : function () {
+    getWebGLErrorMessage : function () {
 
-		var domElement = document.createElement( 'div' );
+	var domElement = document.createElement( 'div' );
 
-		domElement.style.fontFamily = 'monospace';
-		domElement.style.fontSize = '13px';
-		domElement.style.textAlign = 'center';
-		domElement.style.background = '#eee';
-		domElement.style.color = '#000';
-		domElement.style.padding = '1em';
-		domElement.style.width = '475px';
-		domElement.style.margin = '5em auto 0';
+	if ( ! this.webgl ) {
 
-		if ( ! this.webgl ) {
-
-			domElement.innerHTML = window.WebGLRenderingContext ? [
-				'Your graphics card does not seem to support <a href="http://khronos.org/webgl/wiki/Getting_a_WebGL_Implementation">WebGL</a>.<br />',
-				'Find out how to get it <a href="http://get.webgl.org/">here</a>.'
-			].join( '\n' ) : [
-				'Your browser does not seem to support <a href="http://khronos.org/webgl/wiki/Getting_a_WebGL_Implementation">WebGL</a>.<br/>',
-				'Find out how to get it <a href="http://get.webgl.org/">here</a>.'
-			].join( '\n' );
-
-		}
-
-		return domElement;
-
-	},
-
-	addGetWebGLMessage : function ( parameters ) {
-
-		var parent, id, domElement;
-
-		parameters = parameters || {};
-
-		parent = parameters.parent !== undefined ? parameters.parent : document.body;
-		id = parameters.id !== undefined ? parameters.id : 'oldie';
-
-		domElement = Detector.getWebGLErrorMessage();
-		domElement.id = id;
-
-		parent.appendChild( domElement );
+	    domElement.innerHTML = window.WebGLRenderingContext ? [
+		'Your graphics card does not seem to support <a href="http://khronos.org/webgl/wiki/Getting_a_WebGL_Implementation">WebGL</a>.<br />',
+		'<a href="https://www.google.com/chrome">Google Chrome</a> is recommended, or',
+		'find out how to get it <a href="http://get.webgl.org/">here</a>.'
+	    ].join( '\n' ) : [
+		'Your browser does not seem to support <a href="http://khronos.org/webgl/wiki/Getting_a_WebGL_Implementation">WebGL</a>.<br/><br/>',
+		'<a href="https://www.google.com/chrome">Google Chrome</a> is recommended,<br/> or',
+		'find out how to get a WebGL-enabled browser <a href="http://get.webgl.org/">here</a>.'
+	    ].join( '\n' );
 
 	}
+
+	return domElement;
+
+    },
+
+    addGetWebGLMessage : function ( parameters ) {
+
+	var parent, id, domElement;
+
+	parameters = parameters || {};
+
+	parent = parameters.parent !== undefined ? parameters.parent : document.body;
+	id = parameters.id !== undefined ? parameters.id : 'oldie';
+
+	domElement = Detector.getWebGLErrorMessage();
+	domElement.id = id;
+
+	parent.appendChild( domElement );
+
+    }
 
 };
