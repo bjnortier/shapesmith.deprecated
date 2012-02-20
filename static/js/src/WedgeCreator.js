c0474ec6e79a60d4d62608978d983b5613005def
Benjamin Nortier <bjnortier@gmail.com>
2012-02-20 21:26:56 +0000

    Wedge dimensions

--- static/js/src/WedgeCreator.js
+++ static/js/src/WedgeCreator.js
@@ -12,17 +12,19 @@ SS.WedgeCreator = SS.PrimitiveCreator.extend({
 
         this.views = this.views.concat([
             new SS.WedgePreview({model: this}),
-            new SS.DraggableUVCorner({model: this, uKey: 'u1'}),
-            new SS.DraggableU2Corner({model: this}),
+            new SS.DraggableUVCorner({model: this, uKey: 'u2'}),
+            new SS.DraggableUCorner({model: this, uKey: 'u1'}),
+            new SS.WedgeDimensionArrows({model: this}),
+            new SS.WedgeDimensionText({model: this}),
         ]);
         this.trigger('change', this);
     },
 
     mouseDownOnUV: function(corner) {
-        this.activateCorner(corner, SS.UVWHeightCursoid, {model: this, uKey: 'u1'});
+        this.activateCorner(corner, SS.UVWHeightCursoid, {model: this, uKey: 'u2'});
     },
 
-    mouseDownOnU2: function(corner) {
+    mouseDownOnU: function(corner) {
         this.activateCorner(corner);
     },
 
@@ -94,28 +96,99 @@ SS.WedgePreview = SS.PreviewWithOrigin.extend({
 
 });
 
-SS.DraggableU2Corner = SS.DraggableCorner.extend({
+SS.DraggableUCorner = SS.DraggableCorner.extend({
 
     initialize: function(options) {
         SS.DraggableCorner.prototype.initialize.call(this, options);
+        this.uKey = options.uKey || 'u1';
         this.render();
     },
 
     priority: 2,
 
     mouseDown: function() {
-        this.model.mouseDownOnU2(this);
+        this.model.mouseDownOnU(this);
     },
 
     cornerPositionFromModel: function() {
-        return {x: this.model.node.origin.x + this.model.node.parameters.u2,
+        return {x: this.model.node.origin.x + this.model.node.parameters[this.uKey],
                 y: this.model.node.origin.y + this.model.node.parameters.v,
                 z: 0};
     },
 
     updateModelFromCorner: function(position) {
-        var u2 = position.x - this.model.node.origin.x;
-        this.model.node.parameters.u2 = Math.round(u2);
+        var u = position.x - this.model.node.origin.x;
+        this.model.node.parameters[this.uKey] = Math.round(u);
+    },
+
+});
+
+SS.WedgeDimensionArrows = SS.SceneObjectView.extend({
+
+    render: function() {
+        this.clear();
+
+        var origin = this.model.node.origin;
+        var u1 = this.model.node.parameters.u1;
+        var u2 = this.model.node.parameters.u2;
+        var v = this.model.node.parameters.v;
+        var w = this.model.node.parameters.w;
+
+        var u1Dim = SS.createDimArrow(u1, new THREE.Vector3(u1,0,0));
+        var u2Dim = SS.createDimArrow(u2, new THREE.Vector3(u2,0,0));
+        var vDim = SS.createDimArrow(v, new THREE.Vector3(0,v,0));
+        var wDim = SS.createDimArrow(v, new THREE.Vector3(0,0,w));
+        u2Dim.position = new THREE.Vector3(0,v,0);
+        vDim.position = new THREE.Vector3(0,0,0);
+        wDim.position = new THREE.Vector3(u2,v,0);
+        this.sceneObject.add(u1Dim);
+        this.sceneObject.add(u2Dim);
+        this.sceneObject.add(vDim);
+        this.sceneObject.add(wDim);
+
+        this.sceneObject.position = new THREE.Vector3(origin.x, origin.y, origin.z);
+
+        this.postRender();
+    },
+
+});
+
+SS.WedgeDimensionText = SS.DimensionText.extend({
+
+    render: function() {
+        this.clear();
+
+        var origin = this.model.node.origin;
+        var u1 = this.model.node.parameters.u1;
+        var u2 = this.model.node.parameters.u2;
+        var v = this.model.node.parameters.v;
+        var w = this.model.node.parameters.w;
+
+        var that = this;
+        ['u1', 'u2', 'v', 'w'].map(function(dim) {
+            var key = '$' + dim;
+            that[key] = that.addElement('<div class="dimension">' + that.model.node.parameters[dim] + '</div>');
+        });
+
+        this.update();
+    },
+
+    update: function() {
+        var origin = this.model.node.origin;
+        var u1 = this.model.node.parameters.u1;
+        var u2 = this.model.node.parameters.u2;
+        var v = this.model.node.parameters.v;
+        var w = this.model.node.parameters.w;
+      
+        var positions = {u1: new THREE.Vector3(origin.x + u1/2, origin.y, origin.z),
+                         u2: new THREE.Vector3(origin.x + u2/2, origin.y + v, origin.z),
+                         v: new THREE.Vector3(origin.x, origin.y + v/2, origin.z),
+                         w: new THREE.Vector3(origin.x + u2, origin.y + v, origin.z + w/2)};
+        for (key in positions) {
+            var pixelPosition = SS.toScreenCoordinates(positions[key]);
+            this['$' + key].css('left', pixelPosition.x);
+            this['$' + key].css('top', pixelPosition.y);
+        }
     },
 
 });
