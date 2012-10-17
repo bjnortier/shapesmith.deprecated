5eefbb2736b8cb113309cf168e848c4a6f3ff12f
Benjamin Nortier <bjnortier@gmail.com>
2012-10-17 12:50:40 +0100

    Three.js r52

--- nodes/ui/priv/www/js/main.js
+++ nodes/ui/priv/www/js/main.js
@@ -1,9 +1,10 @@
+var SS = SS || {};
 requirejs(['src/scene'], function(scene) {
 
     $(document).ready(function() {
 
         var container = document.getElementById('scene');
-        new scene.Model({container: container});
+        SS.sceneModel = new scene.Model({container: container});
 
     });
 
