2b414eda9ca2182b700b7fb312f0d78a0ba2620b
Benjamin Nortier <bjnortier@gmail.com>
2012-10-17 09:58:12 +0100

    Trackball rotate & and better lighting

--- nodes/ui/priv/www/js/src/scene.js
+++ nodes/ui/priv/www/js/src/scene.js
@@ -1,4 +1,4 @@
-define(function() {
+define(['src/trackball'], function(trackball) {
 
     var Model = Backbone.Model.extend({
 
@@ -15,16 +15,16 @@ define(function() {
 
             this.width = this.$el.width();
             this.height = this.$el.height();
-
             this.scene = new THREE.Scene();
 
-            this.camera = new THREE.PerspectiveCamera(30, this.width/this.height, 1, 10000);
+            // Camera
+            this.camera = new THREE.PerspectiveCamera(30, this.width/this.height, 1, 1000);
             this.camera.up = new THREE.Vector3(0,0,1);
-            this.camera.position = new THREE.Vector3(1000,0,0);
+            this.camera.position = new THREE.Vector3(0,0,1000);
             this.camera.lookAt(new THREE.Vector3(0,0,0));
             this.scene.add(this.camera);
 
-
+            // Renderer
             this.renderer = new THREE.WebGLRenderer({antialias: true});
             this.renderer.autoClear = true;
             this.renderer.setClearColorHex(0x080808, 0.0);
@@ -33,52 +33,34 @@ define(function() {
 
             // Lights
             this.scene.add(new THREE.AmbientLight(0x404040));
-            var pointLight = new THREE.PointLight( 0x999999 );
-            pointLight.position.set(0, 0, 100);
-            this.scene.add(pointLight);
-            pointLight = new THREE.PointLight( 0x999999 );
-            pointLight.position.set(0, 0, -100);
-            this.scene.add(pointLight);
+            this.pointLight1 = new THREE.PointLight( 0x999999 );
+            this.pointLight1.position.set(1000, 1000, 1000);
+            this.scene.add(this.pointLight1);
 
             var materials = [
                 new THREE.MeshLambertMaterial( { ambient: 0xbbbbbb,  side: THREE.DoubleSide } ),
                 new THREE.MeshBasicMaterial( { color: 0xffff00, wireframe: false, transparent: true, opacity: 0.5, side: THREE.DoubleSide } )
             ];
-            var object = THREE.SceneUtils.createMultiMaterialObject( new THREE.CubeGeometry(100, 100, 100, 4, 4, 4), materials );
+            var object = THREE.SceneUtils.createMultiMaterialObject( new THREE.CubeGeometry(50, 50, 50, 4, 4, 4), materials );
             this.scene.add( object );
 
             this.el.appendChild(this.renderer.domElement);
+            this.trackball = new trackball.Trackball();
+            this.animate();
+        },
 
-            this.render();
+        animate: function() {
+            var that = this;
+            var animateFn = function() {
+                requestAnimationFrame(animateFn);
+                that.render();
+            }
+            animateFn();
         },
 
         render: function() {
-            // var azimuth = -1.373, elevation = 1.08;
-            // var target = { azimuth: -1.373, elevation: 1.08 };
-            // var distance = 1000, distanceTarget = 300;
-            // var targetScenePosition = new THREE.Vector3(0,0,0);
-
-            // azimuth += (target.azimuth - azimuth) * 0.2;
-            // elevation += (target.elevation - elevation) * 0.3;
-
-            // var dDistance = (distanceTarget - distance) * 0.3;
-
-            // if (distance + dDistance > 1000) {
-            //     distanceTarget = 1000;
-            //     distance = 1000;
-            // } else if (distance + dDistance < 3) {
-            //     distanceTarget = 3;
-            //     distance = 3;
-            // } else {
-            //     distance += dDistance;
-            // }
-
-            // var lastCameraPosition = this.camera.position.clone();
-
-            // this.camera.position.x = distance * Math.sin(elevation) * Math.cos(azimuth);
-            // this.camera.position.y = distance * Math.sin(elevation) * Math.sin(azimuth);
-            // this.camera.position.z = distance * Math.cos(elevation);
-
+            this.trackball.updateCamera(this.camera);
+            this.pointLight1.position = this.camera.position;
             this.renderer.render(this.scene, this.camera);
         },
 
@@ -86,20 +68,20 @@ define(function() {
 
         events: {
             'mousedown' : 'mousedown',
-            'mouseover' : 'mouseover',
+            'mousemove' : 'mousemove',
             'mouseup'   : 'mouseup',
         },
 
-        mouseup: function() {
-            console.log('mouseup');
+        mouseup: function(event) {
+            this.trackball.mouseup(event);
         },        
 
         mousedown: function() {
-            console.log('mousedown');
+            this.trackball.mousedown(event);
         },        
 
-        mouseover: function() {
-            console.log('mouseover');
+        mousemove: function() {
+            this.trackball.mousemove(event);
         },
 
     });
--- nodes/ui/priv/www/js/src/trackball.js
+++ nodes/ui/priv/www/js/src/trackball.js
@@ -2,7 +2,93 @@ define(function() {
 
     var Trackball = function() {
 
+        this.minDistance = 3;
+        this.maxDistance = 1000;
+        this.panOrRotateThreshold = 10;
+
+        this.position = { azimuth: -1.373, elevation: 1.08, distance: 1000, scenePosition: new THREE.Vector3() };
+        this.target = { azimuth: -1.373, elevation: 1.08, distance: 300, scenePosition: new THREE.Vector3() };
+
     };
+
+    Trackball.prototype.mousedown = function(event) {
+        this.mouseDownPosition = eventToPosition(event);
+        this.targetOnDown = { 
+            azimuth:  this.target.azimuth, 
+            elevation: this.target.elevation,
+        };
+    };    
+
+    Trackball.prototype.mouseup = function(event) {
+        this.mouseDownPosition = undefined;
+        this.state = undefined;
+    };
+
+    Trackball.prototype.mousemove = function(event) {
+
+        if (this.mouseDownPosition) {
+            var eventPosition = eventToPosition(event);
+
+            if (this.state === undefined) {
+                if (this.overThreshold(eventPosition)) {
+                    this.state = 'rotating';
+                }
+            }
+
+            if (this.state === 'rotating') {
+                var zoomDamp = 0.0005 * Math.sqrt(this.position.distance);
+                this.target.azimuth = this.targetOnDown.azimuth - (eventPosition.x - this.mouseDownPosition.x) * zoomDamp;
+                this.target.elevation = this.targetOnDown.elevation - (eventPosition.y - this.mouseDownPosition.y) * zoomDamp;
+                this.target.elevation = this.target.elevation > Math.PI ? Math.PI : this.target.elevation;
+                this.target.elevation = this.target.elevation < 0 ? 0 : this.target.elevation;
+            }
+
+        }
+    };
+
+    Trackball.prototype.updateCamera = function(camera) {
+        var damping = 0.25;
+        this.position.azimuth += (this.target.azimuth - this.position.azimuth) * damping;
+        this.position.elevation += (this.target.elevation - this.position.elevation) * damping;
+
+        var dDistance = (this.target.distance - this.position.distance) * damping;
+        var newDistance = this.position.distance + dDistance;
+        if (newDistance > this.maxDistance) {
+            this.target.distance = this.maxDistance;
+            this.position.distance = this.maxDistance;
+        } else if (newDistance < this.minDistance) {
+            this.target.distance = this.minDistance;
+            this.position.distance = this.minDistance;
+        } else {
+            this.position.distance = newDistance;
+        }
+
+        camera.position.x = this.position.distance * Math.sin(this.position.elevation) * Math.cos(this.position.azimuth);
+        camera.position.y = this.position.distance * Math.sin(this.position.elevation) * Math.sin(this.position.azimuth);
+        camera.position.z = this.position.distance * Math.cos(this.position.elevation);
+
+        camera.lookAt(new THREE.Vector3(0,0,0));
+
+    };
+
+    Trackball.prototype.overThreshold = function(pos2) {
+        if (!this.mouseDownPosition) {
+            return false;
+        }
+        var pos1 = this.mouseDownPosition;
+        var dx = Math.abs(pos1.x - pos2.x);
+        var dy = Math.abs(pos1.y - pos2.y);
+        return Math.sqrt(dx*dx + dy*dy) > this.panOrRotateThreshold;
+    };
+
+    var eventToPosition = function(event) {
+        return {
+            x: event.clientX,
+            y: event.clientY,
+        };
+    }
+
+
 
     return {
         Trackball: Trackball
