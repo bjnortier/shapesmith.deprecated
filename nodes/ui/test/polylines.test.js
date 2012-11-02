acc9ac63404c3d25d60fb62f02e0eb9244f7bd1c
Benjamin Nortier <bjnortier@gmail.com>
2012-11-02 11:19:46 +0000

    More polyline tests

--- nodes/ui/test/polylines.test.js
+++ nodes/ui/test/polylines.test.js
@@ -3,15 +3,17 @@ var chai = require('chai'),
     client = require('./client').client;
 chai.Assertion.includeStack = true;
 
-describe('Points', function() {
+describe('Polylines', function() {
 
     before(function(done) {
+        this.timeout(5000);
         client.init(function() {
             done();
         });
     });
 
     beforeEach(function(done) {
+        this.timeout(2000);
         client
             .url('http://localhost.shapesmith.net:8000/local/testdesign/modeller')
             .execute('SS.dontDampTrackball(); SS.zoomIn(); ', function() {
@@ -26,29 +28,80 @@ describe('Points', function() {
 
     // ---------- Cases ----------
 
-    it('can be created with subsequent mouse clicks', function(done) {
-        this.timeout(10000);
+
+    it('can be created on the workplane', function(done) {
+        this.timeout(5000);
         client
             .click('.toolbar .polyline')
-            .getEditingVertexName(function(name) {
+            .clickOnWorld(0,0,0)
+            .clickOnWorld(10,10,0)
+            .dblClickOnWorld(0,10,0)
+            .click('.toolbar .select')
+            .elements('css selector', '.vertex.editing', function(result) {
+                assert.equal(result.value.length, 0)
                 client
-                    .moveToWorld(20,10,0)
-                    .assertCoordinateEqual('.vertex.editing .coordinate', 20, 10, 0)
-                    .clickOnWorld(20,10,0)
-                    .isVisible('.vertex.display.'+name, function(result) {
-                        assert.isTrue(result);
-                        client
-                            .clickOnWorld(0,0,0)
-                            .clickOnWorld(0,10,0)
-                            .clickOnWorld(0,20,0)
-                            .click('.toolbar .select')
-                            .elements('css selector', '.vertex.display', function(result) {
-                                assert.equal(result.value.length, 4);
-                                done();
-                            });
-
+                    .assertTextEqual('.vertex.display', 'polyline0')
+                    .elements('css selector', '.vertex.display', function(result) {
+                        assert.equal(result.value.length, 1);
+                        done();
                     });
+
             });
     });
 
+    it('can be created by re-using a point', function(done) {
+        this.timeout(5000);
+        client
+            .click('.toolbar .polyline')
+            .clickOnWorld(0,0,0)
+            .clickOnWorld(10,10,0)
+            .clickOnWorld(0,10,0)
+            .clickOnWorld(0,0,0)
+            .assertTextEqual('.vertex.editing.polyline0 .point._0', 'point0')
+            .assertTextEqual('.vertex.editing.polyline0 .point._1', 'point1')
+            .assertTextEqual('.vertex.editing.polyline0 .point._2', 'point2')
+            .assertTextEqual('.vertex.editing.polyline0 .point._3', 'point0')
+            .assertTextEqual('.vertex.editing.polyline0 .point._4', 'point4', done)
+    });
+
+
+    it('can finish on an existing point', function(done) {
+        this.timeout(5000);
+        client
+            .click('.toolbar .point')
+            .clickOnWorld(0,0,0)
+            .click('.toolbar .polyline')
+            .clickOnWorld(0,0,0)
+            .clickOnWorld(10,10,0)
+            .clickOnWorld(0,10,0)
+            .dblClickOnWorld(0,0,0)
+            .click('.toolbar .select')
+            .elements('css selector', '.vertex.display', function(result) {
+                assert.equal(result.value.length, 2);
+                done();
+            });
+    });
+
+    it('can edited by dragging the points', function(done) {
+        this.timeout(5000);
+        client
+            .click('.toolbar .polyline')
+            .clickOnWorld(0,0,0)
+            .clickOnWorld(10,50,0)
+            .clickOnWorld(10,0,0)
+            .clickOnWorld(20,50,0)
+            .clickOnWorld(20,0,0)
+            .clickOnWorld(30,50,0)
+            .dblClickOnWorld(30,0,0)
+            .click('.toolbar .select')
+            .moveToWorld(0,0,0)
+            .buttonDown()
+            .moveToWorld(0,-50,0)
+            .moveToWorld(0,-50,0)
+            .assertCoordinateEqual('.vertex.editing .coordinate', 0, -50, 0)
+            .buttonUp(done)
+    });
+
+
 });
+
